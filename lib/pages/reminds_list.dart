e701fc5bfa4d2bd6fa48223a114dd422931adbc6
Outtech105k <techno510tk@gmail.com>
2024-07-29 00:09:42 +0900

    Add Countdown for departure Google Routes APIによる検索機能の実装

--- lib/pages/reminds_list.dart
+++ lib/pages/reminds_list.dart
@@ -6,12 +6,14 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:intl/intl.dart';
 
 import 'remind.dart';
 import 'remind_edit.dart';
 import '../database/database.dart';
 import '../widgets/separated_stream_list.dart';
+import '../services/search_route.dart';
 
 class RemindsListPage extends StatefulWidget {
   const RemindsListPage({
@@ -28,6 +30,8 @@ class RemindsListPage extends StatefulWidget {
 class _RemindsListPageState extends State<RemindsListPage> {
   RemindWithPlace? _nextRemind;
   Timer? _timer;
+  bool _hasFetchedNextEvent = false;
+  DateTime? _departureDeadline; // 出発リミット目安を保持する変数
 
   @override
   void initState() {
@@ -50,12 +54,16 @@ class _RemindsListPageState extends State<RemindsListPage> {
   }
 
   String _formatDuration(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String twoDigits(int n) => n.abs().toString().padLeft(2, '0');
+    final isNegative = duration.isNegative;
     final hours = duration.inHours;
     final minutes = duration.inMinutes.remainder(60);
     final seconds = duration.inSeconds.remainder(60);
-    return '${twoDigits(hours)}:${twoDigits(minutes)}:${twoDigits(seconds)}';
+    final formattedDuration = '${twoDigits(hours)}:${twoDigits(minutes)}:${twoDigits(seconds)}';
+
+    return isNegative ? '-$formattedDuration' : formattedDuration;
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -66,88 +74,122 @@ class _RemindsListPageState extends State<RemindsListPage> {
             Padding(
               padding: const EdgeInsets.all(10),
               child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: Theme.of(context).brightness == Brightness.dark
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: Theme.of(context).brightness == Brightness.dark
                         ? Colors.white
-                        : Colors.black
-                    ),
+                        : Colors.black,
                   ),
-                  height: 70,
-                  child: StreamBuilder<RemindWithPlace?>(
-                    stream: widget.database.watchNextUpcomingRemind(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
+                ),
+                height: 70,
+                child: StreamBuilder<RemindWithPlace?>(
+                  stream: widget.database.watchNextUpcomingRemind(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
 
-                      if (snapshot.hasData && snapshot.data != null) {
-                        final remind = snapshot.data!.remind;
-                        final now = DateTime.now();
-                        final deadline = remind.deadline;
-                        final remainingTime = deadline.difference(now);
-
-                        return Row(
-                          children: [
-                            Expanded(
-                                child: Column(
-                                  children: [
-                                    Text("次のイベント開始まで"),
-                                    Text(
-                                        _formatDuration(remainingTime),
-                                      style: TextStyle(fontSize: 30),
-                                    ),
-                                  ],
-                              ),
-                            )
-                          ],
-                        );
-                      } else {
-                        return const Center(
-                          child: Text("ここには、直近イベントに関する詳細が表示されます"),
-                        );
+                    if (snapshot.hasData && snapshot.data != null) {
+                      if (!_hasFetchedNextEvent) {
+                        _hasFetchedNextEvent = true;
+                        searchDriveRouteFromCurrent(
+                          LatLng(snapshot.data!.place.latitude, snapshot.data!.place.longitude),
+                          snapshot.data!.remind.deadline,
+                        ).then((routes) {
+                          int seconds = int.parse(routes[0].duration.replaceAll('s', ''));
+                          Duration duration = Duration(seconds: seconds);
+                          setState(() {
+                            _departureDeadline = snapshot.data!.remind.deadline.subtract(duration); // 計算結果をセット
+                          });
+                        });
                       }
-                    },
-                  )
+                      final remind = snapshot.data!.remind;
+                      final now = DateTime.now();
+                      final deadline = remind.deadline;
+                      final remainingTime = deadline.difference(now);
+                      final departureRemainingTime = _departureDeadline?.difference(now);
+
+                      return Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              children: [
+                                const Text("次のイベント開始まで"),
+                                Text(
+                                  _formatDuration(remainingTime),
+                                  style: const TextStyle(fontSize: 30),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                const Text("出発リミット目安まで"),
+                                Text(
+                                  departureRemainingTime != null
+                                      ? _formatDuration(departureRemainingTime)
+                                      : "--:--:--",
+                                  style: TextStyle(
+                                      fontSize: 30,
+                                    color: departureRemainingTime==null || !_formatDuration(departureRemainingTime!).startsWith('-')
+                                      ? Colors.white
+                                        : Colors.red
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      );
+                    } else {
+                      return const Center(
+                        child: Text("ここには、直近イベントに関する詳細が表示されます"),
+                      );
+                    }
+                  },
+                ),
               ),
             ),
-
             Expanded(
-                child: SeparatedStreamList<RemindWithPlace>(
-                  stream: widget.database.watchAllRemindsWithPlaces(),
-                  itemBuilder: (context, record) {
-                    return ListTile(
-                      // リマインド情報の表示
-                      // TODO: 充実化
-                      title: Text(record.remind.name),
-                      subtitle: Text("${DateFormat('yyyy-MM-dd HH:mm').format(record.remind.deadline)}\n${record.place.name}"),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => RemindPage(database: widget.database, remind: record.remind)),
-                        );
-                      },
-                    );
-                  },
-                )
+              child: SeparatedStreamList<RemindWithPlace>(
+                stream: widget.database.watchAllRemindsWithPlaces(),
+                itemBuilder: (context, record) {
+                  return ListTile(
+                    // リマインド情報の表示
+                    // TODO: 充実化
+                    title: Text(record.remind.name),
+                    subtitle: Text(
+                      "${DateFormat('yyyy-MM-dd HH:mm').format(record.remind.deadline)}\n${record.place.name}",
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => RemindPage(
+                            database: widget.database,
+                            remind: record.remind,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
             ),
-
           ],
         ),
-
-
         // リマインダ追加ボタン
-        // TODO: 追加ページ作成・遷移実装
         floatingActionButton: FloatingActionButton(
           tooltip: "リマインダーを追加",
           child: const Icon(Icons.notification_add),
           onPressed: () {
             Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => RemindEditPage(database: widget.database),
-                )
+              context,
+              MaterialPageRoute(
+                builder: (context) => RemindEditPage(database: widget.database),
+              ),
             );
           },
         ),
---
+++ lib/services/search_route.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:location/location.dart';
+
+import 'location.dart';
+import '../secret.dart';
+
+class Polyline{
+  Polyline({
+    required this.encodedPolyline,
+  });
+
+  final String encodedPolyline;
+
+  factory Polyline.fromJson(dynamic json) {
+    return Polyline(
+        encodedPolyline: json["encodedPolyline"] as String,
+    );
+  }
+}
+
+class Route{
+  Route({
+    required this.distanceMeters,
+    required this.duration,
+    required this.polyline,
+  });
+
+  final int distanceMeters;
+  final String duration;
+  final Polyline polyline;
+
+  factory Route.fromJson(dynamic json) {
+    return Route(
+      distanceMeters: json["distanceMeters"] as int,
+      duration: json["duration"] as String,
+      polyline: Polyline.fromJson(json["polyline"]),
+    );
+  }
+}
+
+Future<List<Route>> searchDriveRouteFromCurrent(LatLng arrival, DateTime arriveTime) async {
+
+  const url = "https://routes.googleapis.com/directions/v2:computeRoutes";
+  final headers = {
+    "Content-Type": "application/json",
+    "X-Goog-Api-Key": ApiKey.googleMapApiKey,
+    "X-Goog-FieldMask": "routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline",
+  };
+
+  final _location = Location();
+  final _currentPosition = await _location.getLocation();
+
+  final body = jsonEncode({
+    "origin": {
+      "location": {
+        "latLng": {
+          "latitude": _currentPosition.latitude,
+          "longitude": _currentPosition.longitude
+        }
+      }
+    },
+    "destination": {
+      "location": {
+        "latLng": {
+          "latitude": arrival.latitude,
+          "longitude": arrival.longitude
+        }
+      }
+    },
+    "arrivalTime": arriveTime.toUtc().toIso8601String(),
+    "languageCode": "ja-JP",
+    "units": "METRIC"
+  });
+
+  final response = await http.post(
+    Uri.parse(url),
+    headers: headers,
+    body: body,
+  );
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+    final List<dynamic> routesJson = jsonResponse["routes"];
+    return routesJson.map((dynamic json) => Route.fromJson(json)).toList();
+  } else {
+    return [];
+  }
+}
